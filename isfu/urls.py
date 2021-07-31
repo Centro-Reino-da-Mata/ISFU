69d1a531ca865446d0d7392d2184752f4f2e6b67
Alexander Silvera <alexandersilvera@hotmail.com>
2021-07-30 23:04:53 -0300

    Eliminando la aplicación blog

--- isfu/urls.py
+++ isfu/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     #path('', include('base.urls')),
     path('miembros/', include('django.contrib.auth.urls')),
     path('miembros/', include('miembros.urls')),
-    path('', include('blog.urls')),
+    #path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
