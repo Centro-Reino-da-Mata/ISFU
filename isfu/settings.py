f7a2bd0915f98d622f78559657a6d489cd5d1ab8
Alexander Silvera <alexandersilvera@hotmail.com>
2021-08-02 17:20:50 -0300

    Agregando la aplicación blog y miembros para deploy

--- isfu/settings.py
+++ isfu/settings.py
@@ -80,7 +80,7 @@ if DEBUG:
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "False"
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 if DEVELOPMENT_MODE is True:
     DATABASES = {
