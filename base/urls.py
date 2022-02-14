32dde3fac57c4780cce2abbcc9d368d7bc51f685
Alexander Silvera <alexandersilvera@hotmail.com>
2022-02-14 00:43:36 -0300

    Creando conección con envio de correo electrónicos

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,5 @@ from .views import galeria, ContactCreateView
 
 urlpatterns = [
     path('galeria/', galeria, name='galeria'),
-    path('contacto/', ContactCreateView.as_view(), name='contacto'),
+    path('contacto/', ContactCreateView.as_view(), name='contacto')
 ]
