0d0182c23cb772103800c4ad8a994692597a96c1
Alexander Silvera <alexandersilvera@hotmail.com>
2022-02-23 21:06:51 -0300

    Cambios en archivos de la aplicación blog, por dificultades en manejo de base de datos en producción

--- isfu/settings.py
+++ isfu/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
-    'miembros',
+    #'miembros',
     #'blog',
     'agenda',
     # Aplicacion de terceros
