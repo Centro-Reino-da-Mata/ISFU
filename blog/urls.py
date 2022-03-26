10a310d0c3db34ed53fcba5b9040b2ff8ea13e46
Alexander Silvera <alexandersilvera@hotmail.com>
2022-03-26 12:42:39 -0300

    Arreglos en app blog, archivos views y en urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,14 @@
-# from django.urls import path
-# from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, \
-#     DeletePostView, AddCategoryView, CategoryView, LikeView
-#
-# urlpatterns = [
-#     path('', HomeView.as_view(), name='home'),
-#     path('articulo/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
-#     path('add_post/', AddPostView.as_view(), name='add_post'),
-#     path('add_category/', AddCategoryView.as_view(), name='add_category'),
-#     path('articulo/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
-#     path('articulo/delete/<int:pk>', DeletePostView.as_view(), name='delete_post'),
-#     path('category/<str:cats>/', CategoryView, name='category'),
-#     path('like/<int:pk>', LikeView, name='like_post'),
-# ]
+from django.urls import path
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, \
+    DeletePostView, AddCategoryView, CategoryView, LikeView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('articulo/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
+    path('articulo/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('articulo/delete/<int:pk>', DeletePostView.as_view(), name='delete_post'),
+    path('category/<str:cats>/', CategoryView, name='category'),
+    path('like/<int:pk>', LikeView, name='like_post'),
+]
